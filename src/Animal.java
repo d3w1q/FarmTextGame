66464fd3b9deb5c7244b6de2eb5ffe8e3b2c5645
d3w1q <72358636+d3w1q@users.noreply.github.com>
2023-01-22 02:11:15 +0100

    Aktualizacja 1
    Dodano działającą funkcję sw do pomijania tygodnia oraz stałą cenę hektara, która zwiększa się co cztery tygodnie.

--- src/Animal.java
+++ src/Animal.java
@@ -51,4 +51,25 @@ public class Animal {
             isAdult = true;
         }
     }
+
+    Animal cow = new Animal("cow", 1000, 12, 200, "milk and meat");
+    Animal pig = new Animal("pig", 2500, 8, 150, "meat");
+    Animal chicken = new Animal("chicken", 50, 6, 75, "eggs and meat");
+    Animal sheep = new Animal ("sheep", 80, 7, 200, "Wool, meat");
+    Animal goose = new Animal("goose", 300, 12, 250, "Blood, feather");
+
+    public Animal createAnimal(String name, int growDuration) {
+        switch(name) {
+            case "cow":
+                return new Animal("cow", 100, growDuration, 200, "milk and meat");
+            case "pig":
+                return new Animal("pig", 80, growDuration, 150, "meat");
+            case "chicken":
+                return new Animal("chicken", 50, growDuration, 75, "eggs and meat");
+            default:
+                throw new IllegalArgumentException("Invalid animal name");
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,46 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Farm myFarm = new Farm("Moja farma", 0, 0, 1000);
+        int landPrice = 250;
+        int start_week = 1;
+        int current_week = start_week;
 
         System.out.println("Witaj w grze tekstowej symulującej pracę właściciela farmy!");
         System.out.println("Twoja farma nazywa się " + myFarm.getName() + " i ma rozmiar " + myFarm.getSize() + " hektarów.");
         System.out.println("Posiadasz " + myFarm.getNumberOfBuildings() + " budynków i " + myFarm.getCash() + " PLN.");
         System.out.println("Twoim zadaniem w tej grze jest jak najszybsze osiągnięcie statusu rolnika doskonałego, który posiada co najmniej 20 hektarów, 5 różnych gatunków zwierząt hodowlanych, 5 różnych gatunków upraw i jedzenie dla wszystkich swoich zwierząt na rok.");
 
+        if (current_week == start_week) {
+            System.out.println("Rozpoczynamy grę w pierwszym tygodniu roku 2020!");
+        } else {
+            System.out.println("Gra trwa od pierwszego tygodnia roku 2020");
+        }
         while (true) {
             System.out.println("Co chcesz zrobić? (wpisz help, jeśli potrzebujesz pomocy)");
             String input = scanner.nextLine();
 
             if (input.equals("help")) {
                 System.out.println("Lista dostępnych poleceń:");
-                System.out.println("- buy land [size] [cost]: Zakup pola o podanym rozmiarze i cenie");
+                System.out.println("- buy land [size]: Zakup pola o podanym rozmiarze (cena bazowa wynosi 500 oraz co cztery tygodnie zwiększa się ona co 500)");
                 System.out.println("- buy building [cost]: Zakup budynku za podaną cenę");
                 System.out.println("- buy animal [name] [cost]: Zakup zwierzęcia o podanej nazwie i cenie");
                 System.out.println("- plant [name]: Posadź roślinę o podanej nazwie na twoim polu uprawnym");
                 System.out.println("- harvest [name]: Zbierz roślinę o podanej nazwie");
                 System.out.println("- sell animal [name]: Sprzedaj zwierzę o podanej nazwie");
                 System.out.println("- feed animal [name]: Karm zwierzę o podanej nazwie");
-                System.out.println("- status: Wyświetl stan Twojej farmy");
+                System.out.println("- status: Wyświetl stan Twojej farmy oraz obecny tydzień");
+                System.out.println("- sw: Przejdź do następnego tygodnia");
                 System.out.println("- exit: Wyjdź z gry");
                 continue;
             }
             if (input.startsWith("buy land")) {
                 String[] commands = input.split(" ");
                 int size = Integer.parseInt(commands[2]);
-                int cost = Integer.parseInt(commands[3]);
+                int cost = landPrice * size;
+                if(myFarm.getCash() < cost) {
+                    System.out.println("Nie masz wystarczającej ilości gotówki, aby kupić to pole.");
+                    continue;
+                }
                 myFarm.buyLand(size, cost);
                 System.out.println("Zakupiłeś " + size + " hektarów ziemi za " + cost + " PLN.");
                 continue;
@@ -83,6 +96,16 @@ public class Main {
                 System.out.println("- Rozmiar: " + myFarm.getSize() + " hektarów");
                 System.out.println("- Liczba budynków: " + myFarm.getNumberOfBuildings());
                 System.out.println("- Gotówka: " + myFarm.getCash() + " PLN");
+                System.out.println("- Aktualny tydzień roku 2020: " + current_week);
+                continue;
+            }
+            if (input.equals("sw")) {
+                current_week++;
+                if (current_week % 4 == 0) {
+                    landPrice += 500;
+                    System.out.println("Cena za hektar wzrosła do " + landPrice + " PLN");
+                }
+                System.out.println("Przeskoczono do tygodnia " + current_week);
                 continue;
             }
             if (input.equals("exit")) {
@@ -90,7 +113,7 @@ public class Main {
                 break;
             }
             System.out.println("Nieznane polecenie. Wpisz help, jeśli potrzebujesz pomocy.");
+
         }
     }
 }
-
